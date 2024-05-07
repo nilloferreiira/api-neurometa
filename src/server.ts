8dd901c11550dc8b69efdbd2af9482b4c1441eda
Danillo <danillodana@outlook.com>
2024-05-07 01:48:32 -0300

    erroHandler

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,28 @@
 import fastify from "fastify";
+import fastifyCors from '@fastify/cors'
+import multipart from "@fastify/multipart"
 import { Approve } from "./routes/approve";
 import { uploads } from "./routes/upload-pdf";
-import multipart from "@fastify/multipart"
 import { errorHandler } from "./error-handler";
 import { RegisterUser } from "./routes/register-user";
 import { serializerCompiler, validatorCompiler } from 'fastify-type-provider-zod';
 
-
 const app = fastify();
-// fastifyCors
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
+
+app.register(fastifyCors, {
+    origin: true
+})
 
 app.register(multipart)
 app.register(RegisterUser)
 app.register(uploads)
 app.register(Approve)
 
-// app.setErrorHandler(errorHandler)
+
+app.setErrorHandler(errorHandler)
 
 app.get('/', async () => {
     return 'Hello'
