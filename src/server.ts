1492115a5b9fd800c535802e3e246680e3f51579
Danillo <danillodana@outlook.com>
2024-05-17 15:09:47 -0300

    add host

--- src/server.ts
+++ src/server.ts
@@ -37,6 +37,7 @@ app.get('/', async () => {
 })
 
 app.listen({
+    host: "0.0.0.0", 
     port: process.env.PORT ? Number(process.env.PORT!) : 3333,
 }).then(() => {
     console.log('Http server running!')
