0038c8c52ba82571b529044b5c8ae4d2f7782f61
Danillo <danillodana@outlook.com>
2024-05-17 14:51:24 -0300

    new db

--- prisma/migrations/20240418195029_doctors_table/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateTable
-CREATE TABLE "doctors" (
-    "crm" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "name" TEXT NOT NULL
-);
---
+++ prisma/migrations/20240517175039_connect_to_cloud_db/migration.sql
@@ -0,0 +1,29 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "cpf" TEXT NOT NULL,
+    "rg" TEXT NOT NULL,
+    "gender" TEXT NOT NULL,
+    "phone_number" TEXT NOT NULL,
+    "birth_date" TIMESTAMP(3) NOT NULL,
+    "approved" BOOLEAN NOT NULL DEFAULT false,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "doctors" (
+    "crm" INTEGER NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "doctors_pkey" PRIMARY KEY ("crm")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_cpf_key" ON "users"("cpf");
